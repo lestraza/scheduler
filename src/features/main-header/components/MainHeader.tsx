6cd77a628c12e9aab2c657b545a6438b7daf7402
nlvova <nlvova@megapolis-it.ru>
2024-03-26 13:59:00 +0100

    KAN-18: fixed for MR

--- src/features/main-header/components/MainHeader.tsx
+++ src/features/main-header/components/MainHeader.tsx
@@ -1,4 +1,3 @@
-import { useCallback } from "react";
 import { useDispatch } from "react-redux";
 import {
   Button,
@@ -14,7 +13,7 @@ import { ReactComponent as ChevronRightIcon } from "../../../shared/icons/chevro
 import { colors } from "../../../shared/styles";
 
 import { useAppSelector, yearsSlice } from "../../scheduler/store";
-import { CalendarView, IncDec } from "../../../shared/types";
+import { CalendarView, IncrementDecrement } from "../../../shared/types";
 import { monthsList } from "../../../shared/constants";
 
 const StyledMenuIcon = styled(MenuIcon)`
@@ -30,26 +29,25 @@ export const MainHeader = () => {
   const { calendarViewTab, displayedMonth, displayedYear } = useAppSelector(
     (state) => state.yearsReducer
   );
+  const { setDisplayedMonth, setDispayedYear, setCalendarViewTab } =
+    yearsSlice.actions;
 
-  const onSetCalendarViewTab = useCallback(
-    (value: string) => {
-      dispatch(yearsSlice.actions.setCalendarViewTab(value as CalendarView));
-    },
-    [dispatch]
-  );
+  const onSetCalendarViewTab = (value: string) => {
+    dispatch(setCalendarViewTab(value as CalendarView));
+  };
 
-  const onSetDisplayedMonth = (value: IncDec) => {
-    if (value === IncDec.Inc) {
+  const onSetDisplayedMonth = (value: IncrementDecrement) => {
+    if (value === IncrementDecrement.Inc) {
       const month = displayedMonth === 11 ? 0 : displayedMonth + 1;
-      dispatch(yearsSlice.actions.setDisplayedMonth(month));
+      dispatch(setDisplayedMonth(month));
       if (month === 0) {
-        dispatch(yearsSlice.actions.setDispayedYear(displayedYear + 1));
+        dispatch(setDispayedYear(displayedYear + 1));
       }
     } else {
       const month = displayedMonth === 0 ? 11 : displayedMonth - 1;
-      dispatch(yearsSlice.actions.setDisplayedMonth(month));
+      dispatch(setDisplayedMonth(month));
       if (month === 11) {
-        dispatch(yearsSlice.actions.setDispayedYear(displayedYear - 1));
+        dispatch(setDispayedYear(displayedYear - 1));
       }
     }
   };
@@ -93,13 +91,17 @@ export const MainHeader = () => {
       </Stack>
       {calendarViewTab === CalendarView.Month ? (
         <Stack direction="row" minWidth="80px">
-          <IconButton onClick={() => onSetDisplayedMonth(IncDec.Dec)}>
+          <IconButton
+            onClick={() => onSetDisplayedMonth(IncrementDecrement.Dec)}
+          >
             <ChevronLeftIcon fill={colors.grey.main} />
           </IconButton>
           <Stack minWidth="120px" alignSelf="center">
             {monthsList[displayedMonth]}
           </Stack>
-          <IconButton onClick={() => onSetDisplayedMonth(IncDec.Inc)}>
+          <IconButton
+            onClick={() => onSetDisplayedMonth(IncrementDecrement.Inc)}
+          >
             <ChevronRightIcon fill={colors.grey.main} />
           </IconButton>
         </Stack>
